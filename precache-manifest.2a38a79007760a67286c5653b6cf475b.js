ed309775bc750083067c420cb70ce43a72c2762c
bobpepers <bob.pepers@gmail.com>
2020-11-25 05:41:57 +0100

    Updates

---
+++ precache-manifest.2a38a79007760a67286c5653b6cf475b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd04e626ad973a5828701c5905b40cd9",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ee213f0788d727393e49",
+    "url": "./static/css/main.42cc8b22.chunk.css"
+  },
+  {
+    "revision": "33ffcfde8df133b0d816",
+    "url": "./static/js/2.4398944c.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.4398944c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee213f0788d727393e49",
+    "url": "./static/js/main.ba480cd8.chunk.js"
+  },
+  {
+    "revision": "3db9eefdb19a5998ac7e",
+    "url": "./static/js/runtime-main.553d3b56.js"
+  }
+]);
